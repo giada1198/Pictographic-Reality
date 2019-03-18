72a564fef4e7fc37d09cc3f87823838fcb0d644d
giada1198 <giada.sun@rocketmail.com>
2019-03-18 13:12:48 -0400

    4th update

--- pack.py
+++ pack.py
@@ -1,60 +1,6 @@
 import json, os, sys
 import numpy as np
 
-# ['test', 'train', 'valid']
-# test = []
-# train = []
-# valid = []
-#
-# base_path = 'raw_data'
-#
-# for i in os.listdir(base_path + '/ren_test'):
-#     if i.endswith('.json'):
-#         full_path = '%s/%s' % (base_path + '/ren_test', i)
-#         with open(full_path) as f:
-#             data = json.load(f)
-#             nd1 = []
-#             for d in data:
-#                 nd2 = []
-#                 nd2.append(d[0])
-#                 nd2.append(d[1])
-#                 nd2.append(d[3])
-#                 nd1.append(nd2)
-#             t = np.array(nd1, dtype='int16')
-#             test.append(t)
-#
-# for i in os.listdir(base_path + '/ren_train'):
-#     if i.endswith('.json'):
-#         full_path = '%s/%s' % (base_path + '/ren_train', i)
-#         with open(full_path) as f:
-#             data = json.load(f)
-#             nd1 = []
-#             for d in data:
-#                 nd2 = []
-#                 nd2.append(d[0])
-#                 nd2.append(d[1])
-#                 nd2.append(d[3])
-#                 nd1.append(nd2)
-#             t = np.array(nd1, dtype='int16')
-#             train.append(t)
-#
-# for i in os.listdir(base_path + '/ren_valid'):
-#     if i.endswith('.json'):
-#         full_path = '%s/%s' % (base_path + '/ren_valid', i)
-#         with open(full_path) as f:
-#             data = json.load(f)
-#             nd1 = []
-#             for d in data:
-#                 nd2 = []
-#                 nd2.append(d[0])
-#                 nd2.append(d[1])
-#                 nd2.append(d[3])
-#                 nd1.append(nd2)
-#             t = np.array(nd1, dtype='int16')
-#             valid.append(t)
-#
-# np.savez_compressed('ren.npz', test=test, train=train, valid=valid)
-
 def pack(input_path, output_path):
     output = {}
     for var in ['test', 'train', 'valid']:
@@ -69,7 +15,7 @@ def pack(input_path, output_path):
                     for d in data:
                         stroke = []
                         for s in [0,1,3]:
-                            stroke.appned(d[s])
+                            stroke.append(d[s])
                         strokes.append(stroke)
                     t = np.array(strokes, dtype='int16')
                     output[var].append(t)
